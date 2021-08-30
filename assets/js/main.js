512ffede2da7f77459102e251846717b20aadd9c
yusifquluzada <yquluzade98@gmail.com>
2021-08-30 20:06:47 +0400

    removed jquery slide effect and added dropdown slide,dropdown arrow rotate effect

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,18 @@
 // DROPDOWN CONTAINER SLIDE
-$(".dropdown-container").on("click", function () {
-    $(this).next().slideToggle();
 
+const dropdownContainer = document.querySelector(".dropdown-container");
+const dropdownContainerSecond = document.querySelector(".dropdown-container-2");
+const dropdownContent = document.querySelector(".dropdown-content");
+const dropdownContentSecond = document.querySelector(".dropdown-content-2");
+const dropdownArrow = document.querySelector(".dropdown-arrow");
+const dropdownArrowSecond = document.querySelector(".dropdown-arrow-2");
+
+dropdownContainer.addEventListener("click", function () {
+    dropdownContent.classList.toggle("active");
+    dropdownArrow.classList.toggle("active");
+});
+
+dropdownContainerSecond.addEventListener("click", function () {
+    dropdownContentSecond.classList.toggle("active");
+    dropdownArrowSecond.classList.toggle("active");
 });
