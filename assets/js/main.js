39dc0e447d4b79b055db2ef55d2ef0c1ddab8bdb
yusifquluzada <yquluzade98@gmail.com>
2021-08-31 10:48:52 +0400

    edited slide js code and simplified it

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,11 @@
 // DROPDOWN CONTAINER SLIDE
 
-const dropdownContainer = document.querySelector(".dropdown-container");
-const dropdownContainerSecond = document.querySelector(".dropdown-container-2");
-const dropdownContent = document.querySelector(".dropdown-content");
-const dropdownContentSecond = document.querySelector(".dropdown-content-2");
+const dropdownContainer = document.querySelectorAll(".dropdown-container");
 const dropdownArrow = document.querySelector(".dropdown-arrow");
-const dropdownArrowSecond = document.querySelector(".dropdown-arrow-2");
 
-dropdownContainer.addEventListener("click", function () {
-    dropdownContent.classList.toggle("active");
-    dropdownArrow.classList.toggle("active");
-});
-
-dropdownContainerSecond.addEventListener("click", function () {
-    dropdownContentSecond.classList.toggle("active");
-    dropdownArrowSecond.classList.toggle("active");
-});
+dropdownContainer.forEach(dropDown => {
+    dropDown.addEventListener("click", function () {
+        dropDown.nextElementSibling.classList.toggle("active");
+        dropDown.firstElementChild.classList.toggle("active");
+    })
+})
