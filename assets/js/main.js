c1e56418d0fd6707f8335b95e0b8e44978d2668f
yusifquluzada <yquluzade98@gmail.com>
2021-08-31 11:51:44 +0400

    added testimonials tab menu js

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,31 @@ const dropdownArrow = document.querySelector(".dropdown-arrow");
 
 dropdownContainer.forEach(dropDown => {
     dropDown.addEventListener("click", function () {
+        // dropDown.nextElementSibling.style.height = dropDown.nextElementSibling.scrollHeight + 'px'; // explore scroll height 
         dropDown.nextElementSibling.classList.toggle("active");
         dropDown.firstElementChild.classList.toggle("active");
     })
 })
+
+
+// Testimonials select JS
+
+const testimonialPictures = Array.from(document.querySelectorAll('.testimonials-pictures li'));
+const testimonialTexts = Array.from(document.querySelectorAll('.testimonials-text-sec .testimonials-text'));
+const clearActives = () => {
+    testimonialPictures.forEach(testimonialPicture => {
+        testimonialPicture.classList.remove('active');
+    });
+    testimonialTexts.forEach(testimonialText => {
+        testimonialText.classList.remove('active');
+    });
+}
+testimonialPictures.forEach(testimonialPicture => {
+    testimonialPicture.onclick = function () {
+        clearActives();
+        const targetId = testimonialPicture.getAttribute('data-target') //home
+        const testimonialText = document.getElementById(targetId);
+        testimonialPicture.classList.add('active');
+        testimonialText.classList.add('active');
+    }
+});
