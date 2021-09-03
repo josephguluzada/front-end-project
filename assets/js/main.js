bdb45ff73825a82340acaaabb825874f1ab44b86
yusifquluzada <yquluzade98@gmail.com>
2021-09-04 01:22:44 +0400

    added JS to blog items tab links

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,9 +27,42 @@ const clearActives = () => {
 testimonialPictures.forEach(testimonialPicture => {
     testimonialPicture.onclick = function () {
         clearActives();
-        const targetId = testimonialPicture.getAttribute('data-target') //home
+        const targetId = testimonialPicture.getAttribute('data-target');
         const testimonialText = document.getElementById(targetId);
         testimonialPicture.classList.add('active');
         testimonialText.classList.add('active');
     }
 });
+
+// BLOG ITEMS TAB LINKS JS
+
+const tabLinks = Array.from(document.querySelectorAll(".tabs .tab-link"));
+const blogItems = Array.from(document.querySelectorAll(".blog-items .blog-item-container"));
+const blogItemOnlyOne = document.querySelector(".blog-items .blog-item-container");
+const clearActivesForTabs = () => {
+    tabLinks.forEach(tabLink => {
+        tabLink.classList.remove('active');
+    });
+    blogItems.forEach(blogItem => {
+        blogItem.classList.remove('active');
+    });
+}
+
+tabLinks.forEach(tabLink => {
+    tabLink.addEventListener("click", function (e) {
+        e.preventDefault();
+        clearActivesForTabs();
+        const targetId = tabLink.getAttribute("data-target");
+        const blogItem = document.getElementById(targetId);
+
+        if (targetId !== "all") {
+            tabLink.classList.add('active');
+            blogItem.classList.add('active');
+        } else {
+            tabLink.classList.add('active');
+            blogItems.forEach(item => {
+                item.classList.add("active");
+            })
+        }
+    })
+})
